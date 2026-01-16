8da7de319f76897bea56084ecafc9b1de03de064
christian-branski-vinci <christian.branski@student.vinci.be>
2026-01-16 16:58:11 +0100

    test: if contains '.' or end with .

--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -2,6 +2,17 @@ function validateEmail(email:string) : boolean{
     if (!email.includes('@')){
         return false;
     }
+
+    const index = email.indexOf('@');
+    const partie= email.substring(index+1);
+
+    if (!partie.includes('.')){
+        return false;
+    }
+
+    if (email.endsWith('.')){
+        return false;
+    }
     return true;
 }
 
--- tests/emailValidator.test.ts
+++ tests/emailValidator.test.ts
@@ -1,7 +1,7 @@
 import validateEmail from "../src/emailValidator"
 
 describe('email validator test', ()=>{
-    describe('must conain "@', ()=>{
+    describe('must conain "@" and be followed by a "."', ()=>{
         it('should reject because no @ in mail', ()=>{
             const mail = "mail"
             const result =validateEmail(mail);
@@ -9,8 +9,15 @@ describe('email validator test', ()=>{
             expect(result).toBe(false);
         })
 
-        it('should accept because contains @', ()=>{
+        it('should reject because contains @', ()=>{
             const mail = "mail@";
+
+            const result = validateEmail(mail);
+
+            expect(result).toBe(false)
+        })
+        it('should accpet because contain @ and followed by .', ()=>{
+            const mail = "mail@exemple.com";
 
             const result = validateEmail(mail);
 
